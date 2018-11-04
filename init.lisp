0689ce64ea845fa10f82a80f9d0943a282b67253
Clint Moore <clint@ivy.io>
2018-11-03 19:30:38 -0700

    Oh, damn.

---
+++ init.lisp
@@ -0,0 +1,5 @@
+
+(ql:quickload :sup)
+(in-package :sup)
+(start-server)
+(start-refresh-thread)
--- sup.lisp
+++ sup.lisp
@@ -625,41 +625,41 @@
 
 
 
-(defclass chat-room (hunchensocket:websocket-resource)
-  ((name :initarg :name :initform (error "Name this room!") :reader name))
-  (:default-initargs :client-class 'user))
+;; (defclass chat-room (hunchensocket:websocket-resource)
+;;   ((name :initarg :name :initform (error "Name this room!") :reader name))
+;;   (:default-initargs :client-class 'user))
 
-(defclass user (hunchensocket:websocket-client)
-  ((name :initarg :user-agent :reader name :initform (error "Name this user!"))))
+;; (defclass user (hunchensocket:websocket-client)
+;;   ((name :initarg :user-agent :reader name :initform (error "Name this user!"))))
 
-(defvar *chat-rooms* (list (make-instance 'chat-room :name "/ws/cc")
-                           (make-instance 'chat-room :name "/ws/append")))
+;; (defvar *chat-rooms* (list (make-instance 'chat-room :name "/ws/cc")
+;;                            (make-instance 'chat-room :name "/ws/append")))
 
-(defun find-room (request)
-  (find (hunchentoot:script-name request) *chat-rooms* :test #'string= :key #'name))
+;; (defun find-room (request)
+;;   (find (hunchentoot:script-name request) *chat-rooms* :test #'string= :key #'name))
 
-(pushnew 'find-room hunchensocket:*websocket-dispatch-table*)
+;; (pushnew 'find-room hunchensocket:*websocket-dispatch-table*)
 
-(defun broadcast (room message &rest args)
-  (loop for peer in (hunchensocket:clients room)
-        do (hunchensocket:send-text-message peer (apply #'format nil message args))))
+;; (defun broadcast (room message &rest args)
+;;   (loop for peer in (hunchensocket:clients room)
+;;         do (hunchensocket:send-text-message peer (apply #'format nil message args))))
 
-(defmethod hunchensocket:client-connected ((room chat-room) user)
-  (hunchensocket:send-text-message user "Connecting to the message bridge...")
-  (attach-queue-handler (lambda (message)
-                          (hunchensocket:send-text-message user
-                                                           (format nil "~a~%"
-                                                                   (flexi-streams:octets-to-string
-                                                                    (cl-rabbit:message/body
-                                                                     (cl-rabbit:envelope/message message))))))))
+;; (defmethod hunchensocket:client-connected ((room chat-room) user)
+;;   (hunchensocket:send-text-message user "Connecting to the message bridge...")
+;;   (attach-queue-handler (lambda (message)
+;;                           (hunchensocket:send-text-message user
+;;                                                            (format nil "~a~%"
+;;                                                                    (flexi-streams:octets-to-string
+;;                                                                     (cl-rabbit:message/body
+;;                                                                      (cl-rabbit:envelope/message message))))))))
 
-(defmethod hunchensocket:client-disconnected ((room chat-room) user)
-  (broadcast room "~a has left ~a" (name user) (name room)))
+;; (defmethod hunchensocket:client-disconnected ((room chat-room) user)
+;;   (broadcast room "~a has left ~a" (name user) (name room)))
 
-(defmethod hunchensocket:text-message-received ((room chat-room) user message)
-  (broadcast room "~a says ~a" (name user) message))  
+;; (defmethod hunchensocket:text-message-received ((room chat-room) user message)
+;;   (broadcast room "~a says ~a" (name user) message))  
 
-(defvar *ws-server* (make-instance 'hunchensocket:websocket-acceptor :port 8087))
+;; (defvar *ws-server* (make-instance 'hunchensocket:websocket-acceptor :port 8087))
 
-(defun start-ws-server ()
-  (hunchentoot:start *ws-server*))
+;; (defun start-ws-server ()
+;;   (hunchentoot:start *ws-server*))
